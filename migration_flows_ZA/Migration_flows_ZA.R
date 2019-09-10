a760e6e8fab4e2a25a607ca8cb30854de769ae7b
Emmanuel Olamijuwon <33476964+eolamijuwon@users.noreply.github.com>
2019-09-10 11:31:01 +0200

    Update Migration_flows_ZA.R

--- migration_flows_ZA/Migration_flows_ZA.R
+++ migration_flows_ZA/Migration_flows_ZA.R
@@ -26,7 +26,7 @@ load <- function(pkg){
 packages <- c("tidyverse", "patchwork", "circlize", 
               "chorddiag", "readxl", "maptools", "rgdal",
               "mosaic", "geosphere", "rgeos", "maps",
-              "showtext")
+              "showtext", "plyr", "showtext")
 load(packages)
 
 
@@ -38,11 +38,16 @@ CS2016 <- readstata13::read.dta13("C:\\Users\\eOlamijuwon\\Downloads\\CS2016Pers
 
 ## Data for extracting Country names from codes
 ## based on information available in the data documentation
-country_codes <- read_xlsx ("./Country Codes.xlsx")
+country_codes <- read.table("https://raw.githubusercontent.com/eolamijuwon/TidyTuesday/master/migration_flows_ZA/country_code.csv", 
+                           sep = ",", header=TRUE) %>% 
+                dplyr::select(-c("X"))
 
 ## Geo-Location Data Obtained from
 ## https://developers.google.com/public-data/docs/canonical/countries_csv
-location_data <- read_xlsx ("./location_data.xlsx")
+location_data <- read.table("https://raw.githubusercontent.com/eolamijuwon/TidyTuesday/master/migration_flows_ZA/location_data.csv", 
+                            sep = ",", header=TRUE) %>% 
+                            dplyr::select(-c("X"))
+
 
 
 CS2016_ZA <- CS2016 %>%
@@ -89,7 +94,10 @@ m <- matrix(table(CS2016_ZA$POB, CS2016_ZA$Usual_Province),
         mycolor <- c("#3F9A6F", "#99B841", "#DBB951",
                          "#8D662B", "#8322D6", "#980100",
                          "#01649B", "#787478", "#000000")
-        
+
+        ## Save Plot to PDF
+        pdf("Internal Migration_ZA.pdf") 
+                
         # Base plot
         chordDiagram(
           x = data_long, 
@@ -124,6 +132,7 @@ m <- matrix(table(CS2016_ZA$POB, CS2016_ZA$Usual_Province),
             
           }
         )
+        dev.off()
         
 
 ## International Migration
@@ -133,63 +142,66 @@ CS2016_Immigra <- CS2016 %>%
   droplevels()
 
 
-immi_flows <- data.frame( table(CS2016_Immigra$Citizenship)) %>% 
-  inner_join(country_codes,
-             by = c("Var1" = "Code")) %>% 
-  mutate(Percentage = round(((Freq/sum(Freq))*100), digits = 2)) %>% 
-  arrange(desc(Percentage)) %>% 
-  
-  mutate(Country = replace(Country, which(Country == "Congo"),
-                           "Congo [Republic]")) %>% 
-  
-  mutate(Country = replace(Country, which(Country == "Congo (Democratic Republic of the)"),
-                         "Congo [DRC]")) %>% 
-  mutate(Country = replace(Country, which(Var1 == "GBR"),
-                           "United Kingdom")) %>% 
-  head(n = 20) %>% 
-  inner_join(location_data, by  = c("Country" = "country")) %>% 
-  dplyr::rename(lat_from = latitude,
-         long_from = longitude) %>% 
-  mutate(lat_to = -30.559482,
-         long_to = 22.937506) %>% 
-  mutate(Country = replace(Country, which(Country == "Swaziland"),
-                           "Eswatini")) %>% 
-  mutate(Country = paste0(Country, " - {",  round(Percentage, digits = 1), "%}"))
 
-
-immi_flows$point <- with(immi_flows,
-                           ifelse(Percentage<1, 0.5,
-                                  ifelse(Percentage>1 & Percentage<=3, 0.85,
-                                         ifelse(Percentage>3 & Percentage<=5, 1.2,
-                                                ifelse(Percentage>5 & Percentage <= 15, 1.55,
-                                                       ifelse(Percentage>15 & Percentage<=35, 1.90,
-                                                              ifelse(Percentage>35, 2.25, 0)))))))
+        immi_flows <- data.frame(table(CS2016_Immigra$Citizenship)) %>% 
+          mutate(Var1 = as.character(Var1)) %>% 
+          inner_join(country_codes,
+                     by = c("Var1" = "Code")) %>% 
+          mutate(Percentage = round(((Freq/sum(Freq))*100), digits = 2)) %>% 
+          arrange(desc(Percentage)) %>% 
+          head(n = 20) %>% 
+          mutate(Country = as.character(Country)) %>% 
+          mutate(Country = replace(Country, which(Country == "Congo"),
+                                   "Congo [Republic]")) %>% 
+          
+          mutate(Country = replace(Country, which(Country == "Congo (Democratic Republic of the)"),
+                                 "Congo [DRC]")) %>% 
+          mutate(Country = replace(Country, which(Var1 == "GBR"),
+                                   "United Kingdom")) %>% 
+          inner_join(location_data, by  = c("Country" = "country")) %>% 
+          dplyr::rename(lat_from = latitude,
+                 long_from = longitude) %>% 
+          mutate(lat_to = -30.559482,
+                 long_to = 22.937506) %>% 
+          mutate(Country = replace(Country, which(Country == "Swaziland"),
+                                   "Eswatini")) %>% 
+          mutate(Country = paste0(Country, " - {",  round(Percentage, digits = 1), "%}"))
+        
+        
+        immi_flows$point <- with(immi_flows,
+                                   ifelse(Percentage<1, 0.5,
+                                          ifelse(Percentage>1 & Percentage<=3, 0.85,
+                                                 ifelse(Percentage>3 & Percentage<=5, 1.2,
+                                                        ifelse(Percentage>5 & Percentage <= 15, 1.55,
+                                                               ifelse(Percentage>15 & Percentage<=35, 1.90,
+                                                                      ifelse(Percentage>35, 2.25, 0)))))))
 
 
 fortify.SpatialLinesDataFrame = function(model, data, ...){
   ldply(model@lines, fortify)
 }
 
-routes = gcIntermediate(immi_flows[,c('long_from', 'lat_from')], immi_flows[,c('long_to', 'lat_to')], 200, breakAtDateLine = FALSE, addStartEnd = TRUE, sp=TRUE)
-fortifiedroutes = fortify.SpatialLinesDataFrame(routes) 
-
-#### merge to form great circles 
-routes_count = data.frame('count'=immi_flows$Percentage,
-                          'id'=1:nrow(immi_flows),
-                          'Countries'=immi_flows$Country,
-                          'point' = immi_flows$point)
-greatcircles = merge(fortifiedroutes, routes_count, all.x=T, by='id')
-
-
-
-
-
-worldmap = map_data ("world")
+        routes = gcIntermediate(immi_flows[,c('long_from', 'lat_from')], 
+                                immi_flows[,c('long_to', 'lat_to')], 200, 
+                                breakAtDateLine = FALSE, addStartEnd = TRUE, sp=TRUE)
+        fortifiedroutes = fortify.SpatialLinesDataFrame(routes) 
+        
+        #### merge to form great circles 
+        routes_count = data.frame('count'=immi_flows$Percentage,
+                                  'id'=1:nrow(immi_flows),
+                                  'Countries'=immi_flows$Country,
+                                  'point' = immi_flows$point)
+        greatcircles = merge(fortifiedroutes, routes_count, all.x=T, by='id')
+        
+        
+        
+        
+        
+        worldmap = map_data ("world")
 
 
 wrld<-c(geom_polygon(aes(long,lat,group=group), size = 0.05, colour = "black",
                      fill="#e7e6e3",
-                     # "#DBB951", 
                      data=worldmap))
 
 
@@ -204,6 +216,7 @@ myFont <- "Roboto Condensed"
 
 
 
+## Plot of International Migration Flows
 
 ggplot() + 
   wrld +
@@ -224,12 +237,6 @@ ggplot() +
         axis.text = element_blank(),
         axis.title = element_blank(),
         axis.ticks = element_blank(),
-        # plot.title = element_text(family =  myFont, size = 45, face = "bold",
-        #                           color = "green", 
-        #                           lineheight = 1.3),
-        # plot.subtitle = element_text(family =  myFont, size = 23,
-        #                              colour = "gray"),
-        text = element_text(family =  myFont, size = 6),
         legend.position = c(0.86, 0.52),
         legend.text = element_text(family =  myFont, size = 3.2,
                                    colour = "black"),
@@ -238,16 +245,13 @@ ggplot() +
                                     lineheight = unit(0.9, "pt")),
         legend.background = element_rect(fill=NA),
         legend.key = element_rect(fill=NA,
-                                  color = "black"),
+                                  color = "black",
+                                  size = 0.2),
         legend.key.size = unit(0.35, 'lines'),
         legend.justification=c("left", "center"),
         plot.margin=unit(c(0.3,0.3,0.3,0.3),"cm"),
         plot.background = element_rect("#acd7ec"),
-        
-        
-        plot.caption = element_text(family =  cmr, size = 18,
-                                    color = "white"),
-        panel.grid = element_line("#acd7ec",
+                panel.grid = element_line("#acd7ec",
                                   size = 0.05),
         panel.background = element_rect("#acd7ec")) +
   guides(size = FALSE)+
